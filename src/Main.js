8d2856f94fe1fb7ba0ebc30a390a2ff7a685c6ed
Tatiana Miller <tatiana.miller@proplant-mvi.com>
2025-03-03 10:19:12 +0100

    small changes

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+function Main() {
+  return (
+    <div>
+      <header>
+        <div className="menu">
+          <a>Über uns</a>
+          <a>Referenzen</a>
+          <a>Leistungen</a>
+          <a>Rechtliches</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
