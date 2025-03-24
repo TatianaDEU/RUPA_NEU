014621c495b8289073b7ffa905d12ce7c3b897c0
Tatiana Miller <tatiana.miller@proplant-mvi.com>
2025-03-24 14:00:27 +0100

    ModalWindow added

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,17 @@
+import { useState } from "react";
+import ModalWindow from "./ModalWindow";
+
 function Main() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
       <header>
@@ -9,13 +22,21 @@ function Main() {
           <a>Rechtliches</a>
         </div>
 
-        <button className="btn">Nachricht???</button>
+        <button onClick={handleOpenModal} className="btn">
+          Kontaktieren
+        </button>
+        <ModalWindow show={showModal} onClose={handleCloseModal}>
+          <h2 style={{ color: "#4824ff", fontSize: "40px" }}>Kontakt</h2>
+          <p style={{ fontSize: "22px" }}>
+            Sie können uns per Telefon <br /> oder per E-Mail kontaktieren
+          </p>
+        </ModalWindow>
 
         <a href="/" target="_blank" className="icon instagram"></a>
         <a href="/" target="_blank" className="icon email"></a>
 
         <div className="switch">
-            <div className="theme light"></div>
+          <div className="theme light"></div>
         </div>
       </header>
     </div>
---
+++ src/ModalWindow.js
@@ -0,0 +1,57 @@
+import React, { useState, useCallback, useEffect } from "react";
+import "./ModalWindow.css";
+
+const ModalWindow = ({ show, onClose, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    if (show) {
+      setIsVisible(true);
+      document.addEventListener("keydown", handleKeyDown);
+    } else {
+      const timer = setTimeout(() => setIsVisible(false), 800);
+      document.removeEventListener("keydown", handleKeyDown);
+      return () => clearTimeout(timer);
+    }
+  }, [show, handleKeyDown]);
+
+  useEffect(() => {
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleKeyDown]);
+
+  return (
+    <div
+      className={`modal-backdrop ${show ? "show" : ""}`}
+      style={{ display: isVisible }}
+      onClick={onClose}
+    >
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button
+          onClick={onClose}
+          className="modal-close-button"
+          aria-label="Close modal"
+        ></button>
+        {children}
+        <div style={{ display: "flex" }}>
+          <a href="/" target="_blank" className="social-button"></a>
+          <a className="social telefon">Telefon</a>
+          <a href="/" target="_blank" className="social-button"></a>
+          <a className="social email">E-Mail</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWindow;
