3bb9a3609668ffcd63fd52ff73ef007e685f81c6
Tatiana Miller <tatiana.miller@proplant-mvi.com>
2025-08-12 10:02:02 +0200

    Navigation mobile

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,28 @@
-import { Children, useState } from "react";
+import { Children, useEffect, useState } from "react";
 import ModalWindow from "./ModalWindow";
 import Alle from "./Portfolio/Alle";
 import Buerogebaude from "./Portfolio/Buerogebaude";
 import Bungalow from "./Portfolio/Bungalow";
 import Doppelhaus from "./Portfolio/Doppelhaus";
 import Einfamilienhaus from "./Portfolio/Einfamilienhaus";
+import Bild from "./assets/images/Bild.png";
 
 function Main() {
+  const [isPortrait, setIsPortrait] = useState(
+    window.innerWidth > window.innerHeight
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsPortrait(window.innerWidth > window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   const [showModal, setShowModal] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState("All");
 
@@ -35,38 +51,94 @@ function Main() {
     setShowModal(false);
   };
 
+  const [scroll, setScroll] = useState(0);
+
+  const scrollUp = () => {
+    setScroll(window.screenY);
+  };
+
+  const upButton = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollUp);
+  }, []);
+
+  const toBlock = (targetY) => {
+    window.scrollTo({ top: parseInt(targetY), behavior: "smooth" });
+  };
+
   return (
     <div>
       <header>
-        <div className="menu">
-          <a>Über uns</a>
-          <a>Referenzen</a>
-          <a>Leistungen</a>
-          <a>Rechtliches</a>
-        </div>
+        {isPortrait ? (
+          <div className="navigation">
+            <div className="menu">
+              <a onClick={upButton}>Über uns</a>
 
-        <button onClick={handleOpenModal} className="btn">
-          Kontaktieren
-        </button>
-        <ModalWindow show={showModal} onClose={handleCloseModal}>
-          <h2 style={{ color: "#a31b0f", fontSize: "40px" }}>Kontakt</h2>
-          <p style={{ fontSize: "22px" }}>
-            Sie können uns per Telefon <br /> oder per E-Mail kontaktieren:
-          </p>
-        </ModalWindow>
+              <a
+                onClick={(e) => toBlock(e.target.dataset.target)}
+                data-target="700"
+              >
+                Leistungen
+              </a>
+              <a
+                onClick={(e) => toBlock(e.target.dataset.target)}
+                data-target="1230"
+              >
+                Referenzen
+              </a>
+              <a
+                onClick={(e) => toBlock(e.target.dataset.target)}
+                data-target="1920"
+              >
+                Rechtliches
+              </a>
+              <a
+                onClick={(e) => toBlock(e.target.dataset.target)}
+                data-target="2600"
+              >
+                ...
+              </a>
+            </div>
+            <div className="header-buttons">
+              <button onClick={handleOpenModal} className="btn">
+                Kontaktieren
+              </button>
 
-        <a
-          href="https://www.instagram.com/rupa.bauprojekt?igsh=MWp5aTM2bzBrdnVyMg=="
-          target="_blank"
-          className="icon instagram"
-        ></a>
-        <a href="/" target="_blank" className="icon email"></a>
-
-        <div className="switch">
-          <div className="theme light"></div>
-        </div>
+              <a
+                href="https://www.instagram.com/rupa.bauprojekt?igsh=MWp5aTM2bzBrdnVyMg=="
+                target="_blank"
+                className="icon instagram"
+              ></a>
+              <a href="/" target="_blank" className="icon email"></a>
+            </div>
+          </div>
+        ) : (
+          <div className="navigation">
+            <div className="header-buttons-mobile">
+              <a
+                href="https://www.instagram.com/rupa.bauprojekt?igsh=MWp5aTM2bzBrdnVyMg=="
+                target="_blank"
+                className="icon icon-mobile instagram"
+              ></a>
+              <a
+                href="/"
+                target="_blank"
+                className="icon icon-mobile email"
+              ></a>
+              <a className="icon-menu"></a>
+            </div>
+          </div>
+        )}
       </header>
-
+      <ModalWindow show={showModal} onClose={handleCloseModal}>
+        <h2 style={{ color: "#a31b0f", fontSize: "40px" }}>Kontakt</h2>
+        <p style={{ fontSize: "22px" }}>
+          Sie können uns per Telefon <br /> oder per E-Mail kontaktieren:
+        </p>
+      </ModalWindow>
       <div className="welcome-block">
         <div className="first-block">
           <h1>
@@ -81,6 +153,10 @@ function Main() {
             <span style={{ color: "#a31b0f" }}>nachhaltige</span> und{" "}
             <span style={{ color: "#a31b0f" }}>langlebige</span> Arbeit.
           </h3>
+        </div>
+
+        <div className="main-image-box">
+          <img className="image" src={Bild} draggable="false" alt="" />
         </div>
       </div>
       <div className="service-block" draggeble="false">
@@ -210,6 +286,11 @@ function Main() {
         </div>
       </div>
 
+      <div className="footer">Rupa</div>
+
+      <button className={scroll < 100 ? "" : "btn-up"} onClick={upButton}>
+        Nach oben
+      </button>
     </div>
   );
 }
